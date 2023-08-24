3ae5976c5ac917027619fe51f54ce31c41bd51c4
LeonKennedy2005 <139060906+LeonKennedy2005@users.noreply.github.com>
2023-08-24 10:16:00 +0600

    Tasks done

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,44 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        char openingBracket = '(';
+        char closingBracket = ')';
+        String first = "9a^2";
+        String second = "5b";
+        byte third = 2;
+        char fourth = 'a';
+        byte fifth = 7;
+        char sixth = 'b';
+        String seventh = "4ab";
+        char plus = '+';
+        char minus = '-';
+        char multiply = '*';
+        char divide = '/';
+
+        System.out.print(openingBracket);
+        System.out.print(first);
+        System.out.print(minus);
+        System.out.print(second);
+        System.out.print(plus);
+        System.out.print(third);
+        System.out.print(plus);
+        System.out.print(fourth);
+        System.out.print(minus);
+        System.out.print(fifth);
+        System.out.print(closingBracket);
+        System.out.print(multiply);
+        System.out.print(openingBracket);
+        System.out.print(openingBracket);
+        System.out.print(fourth);
+        System.out.print(plus);
+        System.out.print(sixth);
+        System.out.print(minus);
+        System.out.print(seventh);
+        System.out.print(closingBracket);
+        System.out.print(divide);
+        System.out.print(third);
+        System.out.print(closingBracket);
+
+
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int[] chars = {'c', 'h', 'a', 'r'};
+
+        for (int intValue : chars) {
+            char charValue = (char) intValue;
+            System.out.print(charValue);
+        }
     }
 }
